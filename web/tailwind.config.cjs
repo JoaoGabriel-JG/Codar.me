e47f7509363c72fd8c18c99f970a06e21fabddae
João Gabriel <joao.nitsus@gmail.com>
2022-10-06 20:57:43 -0300

    att

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,7 +5,18 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontSize: {
+      xs: ['12px', '16px'],
+      sm: ['14px', '18px'],
+      base: ['16px', '24px'],
+      lg: ['20px', '28px'],
+      xl: ['24px', '32px'],
+      '2xl': ['32px', '40px'],
+      '3xl': ['32px', '40px'],
+      '4xl': ['48px', '56px'],
+    },
+    extend: {
+    },
   },
   plugins: [],
 }
