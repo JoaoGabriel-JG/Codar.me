3e03f38e80315d9eebcb8560bc4f97e6ce9a4405
João Gabriel <joao.nitsus@gmail.com>
2022-10-08 15:27:25 -0300

    att

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,7 +1,16 @@
 export function App() {
   return (
-    <div className="h-screen bg-red-700 text-white">
-      <h1 className="text-3xl">Dê o seu palpite na Copa do Mundo do Catar 2022!</h1>
+    <div className="h-screen bg-red-700 text-white flex flex-col space-y-6">
+      <h1 className="text-3xl text-center font-bold">
+        Dê o seu palpite na Copa do Mundo do Catar 2022!
+      </h1>
+
+      <button className="text-red-700 bg-white text-xl px-8 py-4 rounded-xl">
+        Criar minha conta
+      </button>
+      <button className="text-white border border-white text-xl px-8 py-4 rounded-xl">
+        Criar minha conta
+      </button>
     </div>
   )
 }
