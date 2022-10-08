1dc81e5b2077d5d37c6e87215bf3dd3cd2b7e0b1
João Gabriel <joao.nitsus@gmail.com>
2022-10-08 18:33:46 -0300

    att

--- web/src/pages/Home/index.jsx
+++ web/src/pages/Home/index.jsx
@@ -1,12 +1,12 @@
-export function App() {
+export function Home() {
   return (
     <div className="h-screen bg-red-700 text-white p-4 flex flex-col items-center space-y-6">
 
-    <header className="p-4">
+    <header className="container flex justify-center max-w-5xl p-4">
       <img src="/imgs/logo/logo-fundo-vinho.svg" className="w-40"/>
     </header>
 
-    <div className="container flex-1 p-4 flex flex-col items-center md:flex-row space-y-6 md:space-y-0 md:space-x-6">
+    <div className="container max-w-5xl flex-1 p-4 flex flex-col items-center md:flex-row space-y-6 md:space-y-0 md:space-x-6">
         
         <div className="md:flex-1 flex justify-center">
           <img src="/imgs/photo.png" className="full max-w-md"/>
@@ -15,12 +15,12 @@ export function App() {
         <div className="md:flex-1 flex flex-col space-y-6">
           <h1 className="text-3xl text-center md:text-left font-bold">Dê o seu palpite na Copa do Mundo do Catar 2022!</h1>
           
-          <button className="text-red-700 bg-white text-lg px-8 py-4 rounded-xl">
+          <a href="/signup" className="text-red-700 bg-white text-lg px-8 py-4 rounded-xl">
             Criar minha conta
-          </button>
-          <button className="text-white border border-white text-xl px-8 py-4 rounded-xl">
+          </a>
+          <a href="/login" className="text-white border border-white text-xl px-8 py-4 rounded-xl">
             Fazer login
-          </button>
+          </a>
         </div>
     </div>
     </div>
